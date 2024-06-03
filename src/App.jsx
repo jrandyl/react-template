b1065e944d2d11d8a3a380d163c06337268dcb06
jrandyl <jonrandyl.calderon@gmail.com>
2024-06-03 17:58:35 +0800

    added routers and page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import './App.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Error404 from './pages/Error404'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Protected from './routes/Protected'
+import { isAuthenticated } from './routes/helpers'
+import Public from './routes/Public'
 
 function App() {
-  
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />
-    },
-    {
-      path: "/about",
-      element: <About />
-    }
-  ])
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route element={<Protected/>}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About/>} />
+        </Route>
+        <Route element={<Public/>}>
+          <Route
+              path="/signin"
+              element={<SignIn />}
+              loader={async () => isAuthenticated()}
+            />
+          <Route
+            path="/signup"
+            element={<SignUp />}
+            loader={async () => isAuthenticated()}
+          />
+          <Route path="*" element={<Error404/>} />
+        </Route>
+       
+      </Route>
+    )
+  );
 
   return (
     <RouterProvider router={router} />
